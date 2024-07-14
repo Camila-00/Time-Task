2b3ce05f7b432d66e7eab28f9248428c3b2325ac
Camila-00 <markcamila1@gmail.com>
2024-07-14 19:55:34 +0800

    dirname

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.set("view engine", "ejs");
-app.set("views", path.join(dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.static(path.join(dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 // Route to render the index.ejs file
