894fcb1ca1ab08b6a6afcb563e9d5a0c9b3a7a00
Camila-00 <markcamila1@gmail.com>
2024-07-14 19:54:10 +0800

    commit3

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
-// Set EJS as the templating engine
-app.set('view engine', 'ejs');
-
-// Serve static files from the "public" directory
-app.use(express.static('public'));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.use(express.static(path.join(dirname, "public")));
+app.use(express.json());
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 // Route to render the index.ejs file
 app.get('/', (req, res) => {
   res.render('index');
